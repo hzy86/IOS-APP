c5de717782a196ae592b0880dafeb5edcea3efed
hzy86 <sixexy@icloud.com>
2019-06-06 17:15:36 -0700

    demo app

---
+++ Automation_Test_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Automation_Test_Demo
+//
+//  Created by Ziyi Huang on 6/6/19.
+//  Copyright © 2019 Ziyi Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Automation_Test_DemoTests/Automation_Test_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Automation_Test_DemoTests.swift
+//  Automation_Test_DemoTests
+//
+//  Created by Ziyi Huang on 6/6/19.
+//  Copyright © 2019 Ziyi Huang. All rights reserved.
+//
+
+import XCTest
+@testable import Automation_Test_Demo
+
+class Automation_Test_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
